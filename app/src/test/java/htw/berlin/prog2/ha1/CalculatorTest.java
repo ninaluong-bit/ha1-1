9a7273a4cfc07e274a34ddc1ae6fe846c99538ac
Nina Luong <luonggninaa@gmail.com>
2025-04-20 19:23:38 +0200

    Commit für neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,24 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+
+    //Subtraktion 
+    @Test
+    @DisplayName("should display result after subtracting two numbers")
+    void testSubtraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey(); 
+
+        String expected = "2"; 
+        String actual = calc.readScreen(); 
+
+        assertEquals(expected, actual); 
+    }
+
+    
 }
 
